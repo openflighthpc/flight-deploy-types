4b349554664ae075c5cb07d3cbf8f981be4135fa
Stu Franks <stu@stuts.uk>
2023-02-01 10:09:02 +0000

    Remove outdated prepare steps

--- openflight-jupyter-standalone/prepare.sh
+++ openflight-jupyter-standalone/prepare.sh
@@ -10,29 +10,9 @@ then
   dnf install -y git
 fi
 
-
-
 dnf install -y python39
 # unsure if the -y option works with pip install
 pip3.9 install jupyterlab 
-
-
-
-# Passwordless SSH for Root
-KEYNAME="/root/.ssh/id_flightprofile"
-if [ ! -f $KEYNAME ]
-then
-  ssh-keygen -t rsa -N '' -f $KEYNAME
-fi
-if ! grep -q "$(cat $KEYNAME.pub)" /root/.ssh/authorized_keys
-then
-  cat $KEYNAME.pub >> /root/.ssh/authorized_keys
-fi
-if ! grep -q "IdentityFile $KEYNAME" /root/.ssh/config
-then
-  echo "Host *" >> /root/.ssh/config
-  echo "  IdentityFile $KEYNAME" >> /root/.ssh/config
-fi
 
 if [ ! -d openflight-jupyter-standalone/.git ]
 then
