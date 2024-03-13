bb903865c7e565836f55d48b72159484500cbb6e
Stu Franks <stu@stuts.uk>
2024-03-13 14:48:48 +0000

    Missing quote

--- openflight-slurm-multinode/prepare.sh
+++ openflight-slurm-multinode/prepare.sh
@@ -28,7 +28,7 @@ fi
 ansible-galaxy collection install 'community.general:>6.6.0' --upgrade
 
 # Ensure jmespath installed for use of json_query, using python ansible is looking at
-PYTHON="$(ansible --version |grep 'python version' |sed 's/.*(//g;s/)//g')
+PYTHON="$(ansible --version |grep 'python version' |sed 's/.*(//g;s/)//g')"
 $PYTHON -m pip install jmespath 
 
 if [ ! -d openflight-slurm-multinode/.git ]
