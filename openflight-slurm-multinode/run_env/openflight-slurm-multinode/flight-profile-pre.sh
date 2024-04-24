c084cdd7f0412bec3eddce92e789abd98f726bb9
Stu Franks <stu@stuts.uk>
2024-04-24 15:04:46 +0100

    Remove unnecessary sub directory in run env

--- openflight-slurm-multinode/run_env/openflight-slurm-multinode/flight-profile-pre.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-ansible-playbook -i $INVFILE --extra-vars="cluster_name=$CLUSTERNAME default_username=$DEFAULT_USERNAME default_user_password=$DEFAULT_PASSWORD access_host=$ACCESS_HOST compute_ip_range=$COMPUTE_IP_RANGE hunter_hosts=$HUNTER_HOSTS ipa_use=$IPA_USE ipa_domain=$IPA_DOMAIN secure_admin_password=$SECURE_ADMIN_PASSWORD configure_monitor=$CONFIGURE_MONITOR" --tags pre $RUN_ENV/openflight-slurm-multinode/main.yml
