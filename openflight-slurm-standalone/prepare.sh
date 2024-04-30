e3aae36ee976749a1bcfba7fe6303ee393f0304c
Stu Franks <stu@stuts.uk>
2024-04-30 16:11:48 +0100

    Fix slurm standalone prepare

--- openflight-slurm-standalone/prepare.sh
+++ openflight-slurm-standalone/prepare.sh
@@ -12,6 +12,5 @@ fi
 
 dnf install -y munge munge-libs perl-Switch numactl flight-slurm flight-slurm-devel flight-slurm-perlapi flight-slurm-torque flight-slurm-slurmd flight-slurm-example-configs flight-slurm-libpmi flight-slurm-slurmctld
 
-# ensure v6.6.0 or above installed for append option in ipa_hostgroup
-ansible-galaxy collection install 'community.general:>6.6.0' --upgrade
-
+# Ensure OpenFlight collection is present
+ansible-galaxy collection install git+https://github.com/openflighthpc/openflight-ansible-collection.git#/openflight/
