34a3fd04d4e5109dab3ab92fbe81dcb88b8bb47d
Stu Franks <stu@stuts.uk>
2024-04-30 10:37:28 +0100

    Use collection for SLURM standalone

--- openflight-slurm-standalone/prepare.sh
+++ openflight-slurm-standalone/prepare.sh
@@ -12,9 +12,6 @@ fi
 
 dnf install -y munge munge-libs perl-Switch numactl flight-slurm flight-slurm-devel flight-slurm-perlapi flight-slurm-torque flight-slurm-slurmd flight-slurm-example-configs flight-slurm-libpmi flight-slurm-slurmctld
 
-if [ ! -d openflight-slurm-standalone/.git ]
-then
-  git clone https://github.com/openflighthpc/openflight-slurm-standalone
-fi
-cd openflight-slurm-standalone
-git pull
+# ensure v6.6.0 or above installed for append option in ipa_hostgroup
+ansible-galaxy collection install 'community.general:>6.6.0' --upgrade
+
