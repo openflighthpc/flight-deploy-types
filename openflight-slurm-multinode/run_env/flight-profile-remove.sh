5969097adfe2636b148fe729adbaeba5dcee48b3
Stu Franks <stu@stuts.uk>
2024-05-06 21:43:50 +0100

    Remove monitor role (still WIP)

--- openflight-slurm-multinode/run_env/flight-profile-remove.sh
+++ openflight-slurm-multinode/run_env/flight-profile-remove.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ansible-playbook -i $INVFILE --extra-vars="cluster_name=$CLUSTERNAME default_username=$DEFAULT_USERNAME default_user_password=$DEFAULT_PASSWORD access_host=$ACCESS_HOST compute_ip_range=$COMPUTE_IP_RANGE hunter_hosts=$HUNTER_HOSTS ipa_use=$IPA_USE ipa_domain=$IPA_DOMAIN secure_admin_password=$SECURE_ADMIN_PASSWORD default_nfs_server=$NFS_SERVER configure_monitor=$CONFIGURE_MONITOR rack_view=$CONFIGURE_RACK_VIEW" --extra-vars="{"remove_node": [$NODE]}" --tags remove $RUN_ENV/remove.yml
+ansible-playbook -i $INVFILE --extra-vars="cluster_name=$CLUSTERNAME default_username=$DEFAULT_USERNAME default_user_password=$DEFAULT_PASSWORD access_host=$ACCESS_HOST compute_ip_range=$COMPUTE_IP_RANGE hunter_hosts=$HUNTER_HOSTS ipa_use=$IPA_USE ipa_domain=$IPA_DOMAIN secure_admin_password=$SECURE_ADMIN_PASSWORD default_nfs_server=$NFS_SERVER rack_view=$CONFIGURE_RACK_VIEW" --extra-vars="{"remove_node": [$NODE]}" --tags remove $RUN_ENV/remove.yml
